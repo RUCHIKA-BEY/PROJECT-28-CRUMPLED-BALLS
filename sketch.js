20e8d90f58159bfd179b9475d688c5ef27f1ad82
RUCHIKA-BEY <85662486+RUCHIKA-BEY@users.noreply.github.com>
2021-09-21 17:05:18 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbin,paper,ground;
+var world;
+
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbin=new Dustbin(1200,650);
+	paper=new Paper(200,450,40);
+	ground=new Ground(width/2,670,width,20);
+
+	
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  dustbin.display();
+  paper.display();
+  ground.display();
+  
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+  
+	}
+}
+
